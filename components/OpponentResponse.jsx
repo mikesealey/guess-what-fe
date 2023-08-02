22b68849c102c34ce828a3e3fe8d2dd72ad14286
connork <connorkelly-95@hotmail.co.uk>
2023-08-02 16:31:53 +0100

    adds aome stuff to handle a response

---
+++ components/OpponentResponse.jsx
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+
+export const OpponentResponse = ({ answer }) => {
+  const positiveResponse = [
+    "Yes!",
+    "How did you know?",
+    "You're right!",
+    "Correct!",
+  ];
+  const negativeResponse = ["Nope!", "No they don't", "False", "Wrong"];
+  const responseDiv = useRef()
+
+  if (answer === null) {
+    
+  }
+
+  return (
+    <div className="opponent-response">
+      <img src="./assets/transparent-speech-bubbles-20.png" className="w-40" />
+      <div ref={responseDiv}>
+
+      </div>
+      {answer && (
+        <p>
+          {
+            positiveResponse[
+              Math.floor(Math.random() * positiveResponse.length)
+            ]
+          }
+        </p>
+      )}
+    </div>
+  );
+};
--- components/QuestionCard.jsx
+++ components/QuestionCard.jsx
@@ -1,41 +1,39 @@
-"use client"
-import { useEffect, useState } from 'react';
-import generateQuestions from '../app/utils/GenerateQuestions';
+"use client";
+import { useEffect, useState } from "react";
+import generateQuestions from "../app/utils/GenerateQuestions";
+import { OpponentResponse } from "./OpponentResponse";
 
-
-export default function QuestionCard({alienObjects, setAlienObjects}) {
-  const [validQuestions, setValidQuestions] = useState({})
-  const [isLoading, setIsLoading] = useState(true)
-  const [indexer, setIndexer] = useState(0)
+export default function QuestionCard({ alienObjects, setAlienObjects }) {
+  const [validQuestions, setValidQuestions] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [indexer, setIndexer] = useState(0);
   const [answer, setAnswer] = useState(null);
-  const [guess, setGuess] = useState()
+  const [guess, setGuess] = useState();
 
+  useEffect(() => {
+    generateQuestions(alienObjects).then((questions) => {
+      setValidQuestions(questions);
+      setIsLoading(false);
+    });
+  }, [alienObjects]);
 
-  useEffect(()=>{
-    generateQuestions(alienObjects).then((questions)=> {
-      setValidQuestions(questions)
-      setIsLoading(false)
-    })
-  }, [alienObjects])
+  const chosenAlien = alienObjects[0];
+  console.log(chosenAlien);
 
-  const chosenAlien = alienObjects[0]
-  console.log(chosenAlien)
-
-  console.log(validQuestions)
+  console.log(validQuestions);
   if (isLoading) {
-    return <h1>loading</h1>
+    return <h1>loading</h1>;
   }
 
   const indexIncrementer = (dir) => {
-    if (dir === +1 && indexer === (validQuestions.length-1)) {
-      setIndexer(indexer - (validQuestions.length -1))
-    } else if (dir === -1 && indexer === 0){
-      setIndexer(validQuestions.length - 1)
+    if (dir === +1 && indexer === validQuestions.length - 1) {
+      setIndexer(indexer - (validQuestions.length - 1));
+    } else if (dir === -1 && indexer === 0) {
+      setIndexer(validQuestions.length - 1);
     } else {
-      setIndexer(indexer + dir)
+      setIndexer(indexer + dir);
     }
-    
-  }
+  };
 
   function questionChecker(alienProp, checkFor) {
     if (chosenAlien[alienProp].toString() === checkFor) {
@@ -46,49 +44,76 @@ export default function QuestionCard({alienObjects, setAlienObjects}) {
   }
 
   function handleSubmit() {
-    questionChecker(validQuestions[indexer].alienProp, validQuestions[indexer].checkFor);
+    questionChecker(
+      validQuestions[indexer].alienProp,
+      validQuestions[indexer].checkFor
+    );
   }
 
-  function submitGuess(e){
-    e.preventDefault()
+  function submitGuess(e) {
+    e.preventDefault();
     // console.log(guess)
-    guessChecker(guess, chosenAlien)
+    guessChecker(guess, chosenAlien);
   }
 
   function guessChecker(guess, chosenAlien) {
     if (guess === chosenAlien._id) {
-      console.log("Winner winner chicken dinner")
+      console.log("Winner winner chicken dinner");
     } else {
-      console.log("Nah,.")
+      console.log("Nah,.");
     }
   }
 
   if (validQuestions.length) {
     return (
-        <div className="questioncard">
-          {/* {validQuestions.map((question) => {
+      <div className="questioncard">
+        {/* {validQuestions.map((question) => {
             return <h3>Does you alien have {question.question}?</h3>
           })} */}
-          <button onClick={()=>{indexIncrementer(-1)}}>back</button>
-          <h3>{validQuestions[indexer].question}</h3>
-          <button onClick={()=>{indexIncrementer(+1)}}>Forward</button>
-          <button onClick={()=>{handleSubmit()}}>Submit</button>
-          <div>{answer ? "Yes" : "No"}</div>
+        <button
+          onClick={() => {
+            indexIncrementer(-1);
+          }}
+        >
+          back
+        </button>
+        <h3>{validQuestions[indexer].question}</h3>
+        <button
+          onClick={() => {
+            indexIncrementer(+1);
+          }}
+        >
+          Forward
+        </button>
+        <button
+          onClick={() => {
+            handleSubmit();
+          }}
+        >
+          Submit
+        </button>
+        <div>{answer ? "Yes" : "No"}</div>
 
-          <form onSubmit={(e)=>{submitGuess(e)}}>
-            <select onChange={(e)=>{setGuess(e.target.value)}}>
+        <form
+          onSubmit={(e) => {
+            submitGuess(e);
+          }}
+        >
+          <select
+            onChange={(e) => {
+              setGuess(e.target.value);
+            }}
+          >
             {alienObjects.map((alien) => {
               if (alien.isActive) {
-                return <option value={alien._id}>{alien._id}</option>
+                return <option value={alien._id}>{alien._id}</option>;
               }
             })}
-            </select>
-            <button>Submit My Guess!</button>
-          </form>
-        </div>
-      );
+          </select>
+          <button>Submit My Guess!</button>
+        </form>
+        <OpponentResponse answer={answer} />
+      </div>
+    );
   }
-
 }
-
-
