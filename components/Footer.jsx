524bdb8cab6b88243f97741956e43eafd70c5def
James Yuill <jamesyuill@jamess-mbp.lan>
2023-08-02 11:12:40 +0100

    adds styling to nav bar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,13 +3,25 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
     <nav>
-      <Link href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+      <Link
+        className="nav-link"
+        href="https://youtu.be/dQw4w9WgXcQ"
+        target="_blank"
+      >
         About Us
       </Link>
-      <Link href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+      <Link
+        className="nav-link"
+        href="https://youtu.be/dQw4w9WgXcQ"
+        target="_blank"
+      >
         Discord
       </Link>
-      <Link href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+      <Link
+        className="nav-link"
+        href="https://youtu.be/dQw4w9WgXcQ"
+        target="_blank"
+      >
         GitHub
       </Link>
     </nav>
