eecd43d0e35cac77772039fe31b6cee560a2bd3f
James Yuill <jamesyuill@jamess-mbp.lan>
2023-08-02 11:52:04 +0100

    updates with user context and onclick functionality for singple player button

--- components/LandingCard.jsx
+++ components/LandingCard.jsx
@@ -1,4 +1,19 @@
+'use client';
+
+import { UserContext } from '@/contexts/User';
+import { useContext } from 'react';
+import { useRouter } from 'next/navigation';
+
 export const LandingCard = () => {
+  const { userOne, setUserOne } = useContext(UserContext);
+
+  const router = useRouter();
+
+  function handleSinglePlayerClick(e) {
+    setUserOne(e.target.value);
+    router.push('/singleplayerdisplay');
+  }
+
   return (
     <div className="landing-text-box">
       <h2>Welcome!</h2>
@@ -13,7 +28,15 @@ export const LandingCard = () => {
           <p>Please enter your name</p>
           <div className="flex flex-col">
             <label htmlFor="username">Username: </label>
-            <input type="text" id="username" required />
+            <input
+              type="text"
+              id="username"
+              value={userOne}
+              onChange={(e) => {
+                setUserOne(e.target.value);
+              }}
+              required
+            />
           </div>
           <div className="flex flex-col">
             <label htmlFor="room-id">Room ID: </label>
@@ -22,8 +45,17 @@ export const LandingCard = () => {
         </div>
 
         <div className="flex gap-4">
-          <button id="singleplayer-btn">1 player</button>
-          <button id="multiplayer-btn">2 player</button>
+          <button
+            id="singleplayer-btn"
+            onClick={(e) => {
+              handleSinglePlayerClick(e);
+            }}
+          >
+            1 player
+          </button>
+          <button id="multiplayer-btn" disabled={true}>
+            2 player
+          </button>
         </div>
       </form>
     </div>
