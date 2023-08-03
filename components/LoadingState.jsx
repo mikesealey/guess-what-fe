1f8a7d557613412dbed4ebc8aa0533e5c0df630c
James Yuill <jamesyuill@jamess-mbp.lan>
2023-08-03 16:05:37 +0100

    updates new loading image

--- components/LoadingState.jsx
+++ components/LoadingState.jsx
@@ -1,16 +1,12 @@
-"use client"
-
 const LoadingState = () => {
+  return (
+    <div className="loading-state-container">
+      <div className="loading-state">
+        <img id="flying-saucer" src="/assets/flying-saucer-joypixels.gif" />
+        <h1 id="loading-text">Loading</h1>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="loading-state-container">
-            <div className="loading-state">
-                <img id="flying-saucer" src="/assets/flyingsaucer.png" />
-                <h1 id="loading-text">Loading</h1>
-            </div>
-        </div>
-    )
-
-}
-
-export default LoadingState
+export default LoadingState;
