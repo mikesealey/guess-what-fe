eb82b22f164c1eb51e96e40867047cbed21a1f3d
Mike <mikesealey@hotmail.com>
2023-08-03 12:22:58 +0100

    adds and styles loading state

--- components/Gameboard.jsx
+++ components/Gameboard.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import AlienCard from './AlienCard';
+import LoadingState from './LoadingState';
 
 export default function Gameboard({ alienObjects, setAlienObjects }) {
-
+  const [isLoading, setIsLoading] = useState(true)
   const alienNames = ["Zorkon", "Xyloph", "Velnar", "Qwixar", "Zyla", "Gorgon", "Xandria", "Vortek", "Azura", "Krellus", "Xalon", "Zephyr", "Krysta", "Ylthar", "Vexxor", "Zyndra", "Drakthar", "Quilrax", "Thelora", "Zygor", "Xarix", "Vylara", "Zindor", "Krognar" ]
 
 
   return (
     <div className="gameboard">
+      {isLoading ? <LoadingState /> : null }
       {alienObjects.map((alien) => {
         alien.name = alienNames[alienObjects.indexOf(alien)]
         return (
@@ -14,7 +17,7 @@ export default function Gameboard({ alienObjects, setAlienObjects }) {
             alien={alien}
             alienObjects={alienObjects}
             setAlienObjects={setAlienObjects}
-            key={alien._id}
+            key={alien._id} 
           />
         );
       })}
