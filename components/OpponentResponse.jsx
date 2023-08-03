3bbc2978874dde124f99fb128d2c82dc4771f62c
connork <connorkelly-95@hotmail.co.uk>
2023-08-03 11:55:36 +0100

    added pop up and styling

--- components/OpponentResponse.jsx
+++ components/OpponentResponse.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { UserContext } from "@/contexts/User";
 
 export const OpponentResponse = ({ answer }) => {
+  const { userOne, setUserOne } = useContext(UserContext);
   const positiveResponse = [
     "Yes!",
     "How did you know?",
@@ -10,34 +12,41 @@ export const OpponentResponse = ({ answer }) => {
   const negativeResponse = ["Nope!", "No they don't", "False", "Wrong"];
   const responseDiv = useRef();
 
-  let checked;
-
-  if (answer === null) {
-  }
+  useEffect(() => {
+    if (answer === null) {
+      responseDiv.current.className = "response-div";
+    } else {
+      responseDiv.current.className = "response-div-show";
+    }
+  }, [answer]);
 
   return (
     <div className="opponent-response">
-      {answer !== null && (
-        <img src="./assets/transparent-speech-bubble.png" className="w-40" />
-      )}
-      {answer && (
-        <p>
-          {
-            positiveResponse[
-              Math.floor(Math.random() * positiveResponse.length)
-            ]
-          }
-        </p>
-      )}
-      {answer !== null && !answer && (
-        <p>
-          {
-            negativeResponse[
-              Math.floor(Math.random() * negativeResponse.length)
-            ]
-          }
-        </p>
-      )}
+      <div className="response-div" ref={responseDiv}>
+        <img src="./assets/speech-bubble-w.png" className="w-40" />
+        <div className="response-text">
+          {answer === null && <p>Welcome {`${userOne}`}</p>}
+          {answer && (
+            <p>
+              {
+                positiveResponse[
+                  Math.floor(Math.random() * positiveResponse.length)
+                ]
+              }
+            </p>
+          )}
+
+          {answer !== null && !answer && (
+            <p>
+              {
+                negativeResponse[
+                  Math.floor(Math.random() * negativeResponse.length)
+                ]
+              }
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
