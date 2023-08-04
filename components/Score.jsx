8367bc8a9ead12973e4113906e19a3b40df12b4e
Matt <matthew.sheehan94@gmail.com>
2023-08-04 15:39:59 +0100

    attempt and abandon timer refactor

--- components/Score.jsx
+++ components/Score.jsx
@@ -2,37 +2,11 @@ import { useState, useContext } from "react";
 import { UserStatsContext } from "@/contexts/UserStats";
 
 export const Score = () => {
-  const [score, setScore] = useState(0);
-  const [hasGuessed, setHasGuessed] = useState(false);
   const { statsObject, setStatsObject } = useContext(UserStatsContext)
-  
-
-  function handleScore() {
-    setScore(score + 1);
-    setHasGuessed(true);
-  }
-
-  function reset(val) {
-    if (val) {
-      setHasGuessed(false);
-    } else {
-      setHasGuessed(true);
-    }
-  }
 
   return (
     <div className="score">
       <p>Score: {statsObject.score}</p>
-      {/* <button onClick={handleScore} disabled={hasGuessed}>
-        Test
-      </button>
-      <button
-        onClick={() => {
-          reset(hasGuessed);
-        }}
-      >
-        Reset
-      </button> */}
     </div>
   );
 };
