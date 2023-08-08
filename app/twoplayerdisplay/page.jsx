9ff106319842543c925c6832cb8da6885bf93fb1
James Yuill <jamesyuill@jamess-mbp.lan>
2023-08-08 21:28:36 +0100

    updates socket with render dot com url

--- app/twoplayerdisplay/page.jsx
+++ app/twoplayerdisplay/page.jsx
@@ -18,7 +18,7 @@ import { ThisUserContext } from '@/contexts/ThisUser';
 
 const { io } = require('socket.io-client');
 
-const socket = io('http://localhost:8080');
+const socket = io('https://guess-what-api.onrender.com/');
 
 export default function TwoPlayerDisplay() {
   const [isGameFinished, setIsGameFinished] = useState(false);
